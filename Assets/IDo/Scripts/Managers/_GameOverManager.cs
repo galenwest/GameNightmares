238d852efa427f6330207e51eab80fa2dfd72569
raohoulin <raohoulin@16801.com>
2020-10-27 17:03:25 +0800

    代码完成

---
+++ Assets/IDo/Scripts/Managers/_GameOverManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class _GameOverManager : MonoBehaviour
+{
+
+    public _PlayerHealth playerHealth;
+    public float restartDelay = 2f;
+    float restartTimer;
+
+    Animator anim;
+    Button btn;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnReLoadScene()
+    {
+        SceneManager.LoadScene("IDo/Scenes/Main");
+    }
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0 && btn == null)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                btn = GetComponentInChildren<Button>();
+                if (btn != null)
+                {
+                    btn.onClick.AddListener(OnReLoadScene);
+                }
+            }
+        }
+    }
+}
